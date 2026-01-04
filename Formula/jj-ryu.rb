d8aec19efc0b59412f0331c53dfa11814eabb005
Dillon Mulroy <dillon@cloudflare.com>
2026-01-04 00:53:20 -0500

    Update jj-ryu to v0.0.1-alpha.6 (#12)

--- Formula/jj-ryu.rb
+++ Formula/jj-ryu.rb
@@ -1,8 +1,8 @@
 class JjRyu < Formula
   desc "Stacked PRs for Jujutsu with GitHub/GitLab support"
   homepage "https://github.com/dmmulroy/jj-ryu"
-  url "https://github.com/dmmulroy/jj-ryu/archive/refs/tags/v0.0.1-alpha.5.tar.gz"
-  sha256 "260eae65c1879a0e2c52a0543e5172004847ca7bc884c8dd379fdddd8eb18b0f"
+  url "https://github.com/dmmulroy/jj-ryu/archive/refs/tags/v0.0.1-alpha.6.tar.gz"
+  sha256 "97438e2e4ab90d888945088bde128109e35421edb8662e8f83875b38724e5d5d"
   license "MIT"
 
   depends_on "rust" => :build
