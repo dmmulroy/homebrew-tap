e377996a467e94f523850be3ba19edbaf7b1d916
dmmulroy <2755722+dmmulroy@users.noreply.github.com>
2026-05-13 12:57:58 +0000

    Update jj-starship to v0.7.1

--- Formula/jj-starship.rb
+++ Formula/jj-starship.rb
@@ -1,8 +1,8 @@
 class JjStarship < Formula
   desc "Unified Starship prompt module for Git and Jujutsu repositories"
   homepage "https://github.com/dmmulroy/jj-starship"
-  url "https://github.com/dmmulroy/jj-starship/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "df18a8c260393671e76af71bb425556c7848769a1f30cb0cf05fde24e73ca021"
+  url "https://github.com/dmmulroy/jj-starship/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "263fc18bac72541f4aa687b44ba8227731be2414f64a039fd752dd52e2ece651"
   license "MIT"
 
   depends_on "rust" => :build
