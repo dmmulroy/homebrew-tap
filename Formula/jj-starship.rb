6abfeb0c1e76d40088b7e30962766192dc925158
Dillon Mulroy <dillon.mulroy@gmail.com>
2026-09-04 10:36:51 -0400

    Update jj-starship to v0.7.3 (#25)
    Update the Homebrew formula URL and checksum for the successfully published v0.7.3 release.

--- Formula/jj-starship.rb
+++ Formula/jj-starship.rb
@@ -1,8 +1,8 @@
 class JjStarship < Formula
   desc "Unified Starship prompt module for Git and Jujutsu repositories"
   homepage "https://github.com/dmmulroy/jj-starship"
-  url "https://github.com/dmmulroy/jj-starship/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "5baba90fa6377dd8f35a403fc2d20dc8d29133b0fdf1f2046dc9b8fe1689d10e"
+  url "https://github.com/dmmulroy/jj-starship/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "acad3bfc4a865561fc0ccf10a75d7a7046342c63bb7e263a6a39cf73d8efb014"
   license "MIT"
 
   depends_on "rust" => :build
